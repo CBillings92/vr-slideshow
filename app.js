ce3362abef1dfc39e4f18b8d9ff6195778aad3e0
CBillings92 <Cameron.billings92@gmail.com>
2017-04-09 13:31:54 -0400

    adding files to github to switch computers -socketIO working, dependencies pulled in, bower init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -17,13 +18,16 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+// app.all('/*', function(req, res, next) {
+//     res.sendFile(__dirname + '/views/index.html');
+// });
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
